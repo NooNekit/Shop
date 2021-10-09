3cbb5b54cbf79f1a6367677c4c865f42e60974bf
Nikita <nhvesenko@gmail.com>
2021-10-09 11:06:04 +0300

    'css'

--- main.js
+++
@@ -1,7 +0,0 @@
-let button = document.getElementById('burger');
-
-let navigation = document.getElementById('menu');
-
-button.addEventListener('click', function () {
-    navigation.classList.toggle('display-block');
-});
