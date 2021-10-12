c6b063fd385d5d60b5d249094eb103d9eb0dd28d
Nikita <nhvesenko@gmail.com>
2021-10-12 22:42:47 +0300

    'scss'

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ let button = document.getElementById('burger');
 
 let navigation = document.getElementById('menu');
 
+let wrp = document.getElementById('wrp');
+
 button.addEventListener('click', function () {
-    navigation.classList.toggle('display-block');
+	navigation.classList.toggle('display-block');
+	wrp.classList.toggle('display-block');
 });
